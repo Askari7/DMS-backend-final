66c6a7ba5ac74de36b81412db76f57dfb206a407
salar <jafrisalar42@gmail.com>
2024-02-24 14:00:58 -0300

    handle large files commit

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ const app = express();
 
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // parse application/json
-app.use(bodyParser.json());
 
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
