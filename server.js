121902fb42acdddb39403ed16006728d655a2fff
askari7 <askariabbaas135@gmail.com>
2024-04-24 13:42:41 +0500

    changes

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 
  app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));});
-const server = app.listen(8083, "0.0.0.0", () => {
+const server = app.listen(8083, "127.0.0.1", () => {
   const { address, port } = server.address();
   console.log("DMS app listening at http://%s:%s", address, port);
 });
