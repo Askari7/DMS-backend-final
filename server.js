886341a158c49a54cce0d8c181997f4f245e2590
askari7 <askariabbaas135@gmail.com>
2024-05-04 14:37:15 +0500

    committed

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 
  app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));});
-const server = app.listen(8083, "54.81.250.98", () => {
+const server = app.listen(8083, "0.0.0.0", () => {
   const { address, port } = server.address();
   console.log("DMS app listening at http://%s:%s", address, port);
 });
